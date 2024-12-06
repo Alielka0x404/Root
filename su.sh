24e220f7a90431be6c1476c87e9963e7bab20f79
Alielka0x404 <alielkamili8@gmail.com>
2024-12-06 17:29:41 +0100

    Add files via upload

---
+++ su.sh
@@ -0,0 +1,13 @@
+
+echo "Please Wait"
+echo "===================================="
+wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+unzip akuh.zip
+unzip root.zip
+tar -xvf root.tar.xz
+rm -rf akuh.zip root.zip root.tar.xz
+clear
+echo "===================================="
+echo "You Got Root access now"
+echo "===================================="
+./dist/proot -S . /bin/bash
